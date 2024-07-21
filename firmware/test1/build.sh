f7b8a7a30cbc2b4ddcade975767cdabf51e00e96
Benjamin Koch <bbbsnowball@gmail.com>
2024-07-21 19:43:36 +0200

    fixup for build script

--- firmware/test1/build.sh
+++ firmware/test1/build.sh
@@ -1,9 +1,11 @@
 set -xe
 #ELF=target/thumbv6m-none-eabi/debug/analog-noise-test1
-ELF=target/thumbv6m-none-eabi/debug/main
 #export RUSTC_LOG=rustc_codegen_ssa::back::link=info
 cargo build
-cargo objdump -- -xd >"$ELF.map"
-cargo nm -- --print-size --size-sort --demangle --radix=d | tail
-cargo size -- -d
-cargo objcopy -- -O binary "$ELF.bin"
+for prog in main main2 ; do
+    ELF=target/thumbv6m-none-eabi/debug/$prog
+    cargo objdump --bin $prog -- -xd >"$ELF.map"
+    cargo nm --bin $prog -- --print-size --size-sort --demangle --radix=d | tail -n 5 | sed 's/^\w* /\t\t/'
+    cargo size --bin $prog -- -d
+    cargo objcopy --bin $prog -- -O binary "$ELF.bin"
+done
