cef3a10286f3de73f09bb0144db895d22487ea5b
Benjamin Koch <bbbsnowball@gmail.com>
2024-08-03 22:43:40 +0200

    add more exotic waveforms

--- firmware/test1/src/gen_sine.py
+++ firmware/test1/src/gen_sine.py
@@ -6,8 +6,13 @@ headroom = 256  # e.g. for adding noise
 amplitude = 2048 - headroom
 x = []
 for i in range(n):
-    y = int(offset + amplitude*math.sin(i*2*math.pi/n))
-    x.append(min(4095-headroom, max(headroom, y)))
+    if n/2 <= i <= 3*n/4 and True:
+        y = offset - amplitude
+    elif n/4 <= i <= 3*n/4 and True:
+        y = offset + amplitude - (i-n/4)*2*amplitude/(n/2)
+    else:
+        y = offset + amplitude*math.sin(i*2*math.pi/n)
+    x.append(min(4095-headroom, max(headroom, int(y))))
 #print(repr(x))
 
 per_line = 16
